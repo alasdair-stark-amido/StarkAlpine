93a3c634b42e5c6fa3dec30b976ca22f36908b87
Alasdair Stark <alasdair.stark@amido.com>
2021-08-24 16:07:06 +0100

    Add simple (and bad) example of a SpecFlow test

---
+++ StarkAlpine.LiftStatus.Business.Specs/Features/LiftStatus.feature
@@ -0,0 +1,6 @@
+﻿Feature: LiftStatus
+Simple service that returns the status of different lifts in a resort
+
+Scenario: Get lifts
+	When I ask for a collection of lifts
+	Then a collection of lifts are returned with their current status
---
+++ StarkAlpine.LiftStatus.Business.Specs/Steps/LiftStatusSteps.cs
@@ -0,0 +1,32 @@
+﻿using StarkAlpine.LiftStatus.Business.Models;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace StarkAlpine.LiftStatus.Business.Specs.Steps
+{
+    [Binding]
+    public class LiftStatusSteps : TechTalk.SpecFlow.Steps
+    {
+        private readonly ILiftStatusService _liftStatusService;
+        private IEnumerable<Lift> _lifts { get; set; }
+
+        public LiftStatusSteps()
+        {
+            _liftStatusService = new LiftStatusService();
+            _lifts = new List<Lift>();
+        }
+
+        [When(@"I ask for a collection of lifts")]
+        public void WhenIAskForACollectionOfLifts()
+        {
+            this._lifts = this._liftStatusService.GetLifts();
+        }
+        
+        [Then(@"a collection of lifts are returned with their current status")]
+        public void ThenACollectionOfLiftsAreReturnedWithTheirCurrentStatus()
+        {
+            Assert.NotEmpty(_lifts);
+        }
+    }
+}
